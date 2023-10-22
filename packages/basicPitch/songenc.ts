12d47c4b75e73a775674b002d8f427ce47b3f817
JEAN GAL <jgatbkk@gmail.com>
2023-10-21 23:36:21 -0400

    Implementing async function for audio to midi

---
+++ packages/basicPitch/songenc.ts
@@ -0,0 +1,63 @@
+import { BasicPitch } from '@spotify/basic-pitch';
+import { AudioBuffer, AudioContext } from 'web-audio-api';
+import * as tf from '@tensorflow/tfjs';
+import fs from 'fs';
+
+import {
+  addPitchBendsToNoteEvents,
+  noteFramesToTime,
+  outputToNotesPoly,
+} from './toMidi';
+
+async function songTrad() {
+  const audioCtx = new AudioContext();
+  let audioBuffer = undefined;
+  
+  audioCtx.decodeAudioData(
+    fs.readFileSync(`${__dirname}/songs/prod.mp3`,),
+    async (_audioBuffer: AudioBuffer) => {
+      audioBuffer = _audioBuffer;
+    },
+    () => {},
+  );
+  
+  while (audioBuffer === undefined) {
+    await new Promise(r => setTimeout(r, 1));
+  }
+  
+  const frames: number[][] = [];
+  const onsets: number[][] = [];
+  const contours: number[][] = [];
+  let pct: number = 0;
+  
+  const basicPitch = new BasicPitch(tf.loadGraphModel(`file://${__dirname}/model/model.json`));
+
+  await basicPitch.evaluateModel(
+    audioBuffer as unknown as AudioBuffer,
+    (f: number[][], o: number[][], c: number[][]) => {
+      frames.push(...f);
+      onsets.push(...o);
+      contours.push(...c);
+    },
+    (p: number) => {
+      pct = p;
+    },
+  );
+
+  const notes = noteFramesToTime(
+    addPitchBendsToNoteEvents(
+      contours,
+      outputToNotesPoly(frames, onsets, 0.25, 0.25, 5),
+    ),
+  );
+
+  const notesTwo = noteFramesToTime(
+    addPitchBendsToNoteEvents(
+      contours,
+      outputToNotesPoly(frames, onsets, 0.5, 0.3, 5, true, null, null, false),
+    ),
+  );
+
+  console.log(notes);
+  console.log(notesTwo);
+}
--- packages/nextjs/pages/registersong.tsx
+++ packages/nextjs/pages/registersong.tsx
@@ -1,40 +1,6 @@
 import { NextPage } from "next";
-import { AudioContext } from "web-audio-api";
 
 const RegisterSong: NextPage = () => {
-  const audioCtx = new AudioContext();
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  let audioBuffer = undefined;
-
-  const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files?.[0];
-    console.log(file);
-  };
-
-  function filetomidi(event: React.MouseEvent<HTMLButtonElement, MouseEvent>): void {
-    const fileInput = event.target as HTMLInputElement;
-    const file = fileInput.files?.[0];
-    if (!file) {
-      return;
-    }
-
-    const reader = new FileReader();
-    reader.onload = async () => {
-      const audioData = reader.result as ArrayBuffer;
-      console.log(audioData);
-      console.log(typeof audioData);
-      audioCtx.decodeAudioData(
-        audioData,
-        async (_audioBuffer: AudioBuffer) => {
-          audioBuffer = _audioBuffer;
-        },
-        // eslint-disable-next-line @typescript-eslint/no-empty-function
-        () => {},
-      );
-    };
-    reader.readAsArrayBuffer(file);
-  }
-
   return (
     <div className="flex items-center flex-col flex-grow pt-10 justify-center items-center">
       <div className="px-5">
@@ -43,8 +9,8 @@ const RegisterSong: NextPage = () => {
         </h1>
       </div>
       <div>
-        <input type="file" accept="audio/*" onChange={handleFileUpload} />
-        <button onClick={filetomidi}>Upload</button>
+        <input type="file" accept="audio/*" />
+        <button>Upload</button>
       </div>
     </div>
   );
