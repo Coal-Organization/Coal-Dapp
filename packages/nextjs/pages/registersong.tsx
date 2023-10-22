4b0ac44320d1ba1f0ad052118fa27bcc52a0c53c
JEAN GAL <jgatbkk@gmail.com>
2023-10-21 20:09:26 -0400

    small changes

--- packages/nextjs/pages/registersong.tsx
+++ packages/nextjs/pages/registersong.tsx
@@ -39,7 +39,7 @@ const RegisterSong: NextPage = () => {
     <div className="flex items-center flex-col flex-grow pt-10 justify-center items-center">
       <div className="px-5">
         <h1 className="text-center mb-8">
-          <span className="block text-4xl font-bold"> Register your Song</span>
+          <span className="block text-4xl font-bold">Register your Song</span>
         </h1>
       </div>
       <div>
