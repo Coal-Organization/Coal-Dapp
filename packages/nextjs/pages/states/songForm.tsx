963ff63a9e5041064dd7c0c743e15ec61448fcf9
JEAN GAL <jgatbkk@gmail.com>
2023-12-06 12:59:15 -0500

    - Added usePrepareContractWrite with proper arguments - Interacts with newly updated loopist.sol file

--- packages/nextjs/pages/states/songForm.tsx
+++ packages/nextjs/pages/states/songForm.tsx
@@ -1,6 +1,12 @@
 import React, { ChangeEvent, FormEvent, useState } from "react";
+import loopistABI from "../../generated/deployedContracts";
+import { useAccount, useContractRead, useContractWrite, usePrepareContractWrite } from "wagmi";
+import type { Address } from "wagmi";
 
 export const SongForm = () => {
+  const { address } = useAccount();
+  const [songId, setSongId] = useState(0);
+  const [metadata, setMetadata] = useState("");
   const [form, setForm] = useState({
     name: "",
     genre: "",
@@ -9,6 +15,61 @@ export const SongForm = () => {
     artists: [""],
     nature: "",
   });
+
+  // Contract
+  const contract = loopistABI[31337][0].contracts.Loopist;
+  const addr = address as Address;
+
+  // Read Contract
+  const {} = useContractRead({
+    address: contract.address,
+    abi: contract.abi,
+    functionName: "getCurrentSongId",
+    onSuccess(data) {
+      setSongId(Number(data));
+    },
+    onError(err) {
+      console.log(err);
+    },
+  });
+
+  const getArgs = () => {
+    if (!addr) {
+      return;
+    }
+    const args = [addr, metadata, [{ songId: songId, shares: 100 }]];
+    return args;
+  };
+
+  // Write Contract
+  const { config } = usePrepareContractWrite({
+    address: contract.address,
+    abi: [
+      {
+        name: "addSong",
+        type: "function",
+        stateMutability: "nonpayable",
+        inputs: [
+          { internalType: "address", name: "author", type: "address" },
+          { internalType: "string", name: "metadata", type: "string" },
+          {
+            internalType: "struct Loopist.Copyright[]",
+            name: "copyrights",
+            type: "tuple[]",
+            components: [
+              { internalType: "uint256", name: "songId", type: "uint256" },
+              { internalType: "uint256", name: "shares", type: "uint256" },
+            ],
+          },
+        ],
+        outputs: [],
+      },
+    ],
+    functionName: "addSong",
+    args: getArgs(),
+  });
+
+  const { write } = useContractWrite(config);
 
   const jsonToIpfs = async (
     id: number,
@@ -50,7 +111,7 @@ export const SongForm = () => {
     e.preventDefault();
     //fetch id from smart contract
     const res = await jsonToIpfs(
-      3,
+      songId,
       form.name,
       form.genre,
       form.author,
@@ -59,36 +120,40 @@ export const SongForm = () => {
       form.nature,
     );
     console.log(res.IpfsHash);
+    setMetadata(res.IpfsHash);
   };
 
   return (
-    <form onSubmit={handleSubmit} className="flex flex-col space-y-4 text-black">
-      <input type="text" name="name" onChange={handleChange} placeholder="Name" />
-      <select name="genre" onChange={handleChange} placeholder="Genre">
-        <option value="">Select genre</option>
-        <option value="pop">Pop</option>
-        <option value="rap">Rap</option>
-        <option value="rock">Rock</option>
-        <option value="jazz">Jazz</option>
-        <option value="blues">Blues</option>
-        <option value="country">Country</option>
-      </select>
-      <input type="text" name="author" onChange={handleChange} placeholder="Author" />
-      <input type="text" name="artists" onChange={handleChange} placeholder="Artists (comma separated)" />
-      <input type="text" name="contactInfo" onChange={handleChange} placeholder="Contact Info" />
-      <select name="nature" onChange={handleChange} className="border p-2">
-        <option value="">Select nature</option>
-        <option value="song">Song</option>
-        <option value="lyrics">Lyrics</option>
-        <option value="both">Both</option>
-      </select>
-      <button
-        className="bg-blue-600 hover:border-white-700 text-white font-bold py-2 px-4 rounded"
-        onClick={() => handleSubmit}
-      >
-        Upload
-      </button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit} className="flex flex-col space-y-4 text-black">
+        <input type="text" name="name" onChange={handleChange} placeholder="Name" />
+        <select name="genre" onChange={handleChange} placeholder="Genre">
+          <option value="">Select genre</option>
+          <option value="pop">Pop</option>
+          <option value="rap">Rap</option>
+          <option value="rock">Rock</option>
+          <option value="jazz">Jazz</option>
+          <option value="blues">Blues</option>
+          <option value="country">Country</option>
+        </select>
+        <input type="text" name="author" onChange={handleChange} placeholder="Author" />
+        <input type="text" name="artists" onChange={handleChange} placeholder="Artists (comma separated)" />
+        <input type="text" name="contactInfo" onChange={handleChange} placeholder="Contact Info" />
+        <select name="nature" onChange={handleChange} className="border p-2">
+          <option value="">Select nature</option>
+          <option value="song">Song</option>
+          <option value="lyrics">Lyrics</option>
+          <option value="both">Both</option>
+        </select>
+        <button
+          className="bg-blue-600 hover:border-white-700 text-white font-bold py-2 px-4 rounded"
+          disabled={!write}
+          onClick={() => handleSubmit}
+        >
+          Upload
+        </button>
+      </form>
+    </div>
   );
 };
 
